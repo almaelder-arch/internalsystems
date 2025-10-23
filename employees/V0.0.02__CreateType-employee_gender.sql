822f4f1e4cb3d631a56788b8a77a9779868d6184
Alma Elder <almaelder83@gmail.com>
2025-10-23 10:51:50 +0100

    correcting sytax error

--- employees/V0.0.02__CreateType-employee_gender.sql
+++ employees/V0.0.02__CreateType-employee_gender.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type pt JOIN pg_namespace pns ON pt.typnamespace = pns.oid WHERE pt.typname ='employee_gender' AND pns.nspname = 'employees'; ) THEN
+    IF NOT EXISTS (SELECT 1 FROM pg_type pt JOIN pg_namespace pns ON pt.typnamespace = pns.oid WHERE pt.typname ='employee_gender' AND pns.nspname = 'employees' ) THEN
 		CREATE TYPE employees.employee_gender AS ENUM
 		('M', 'F');
 	END IF;

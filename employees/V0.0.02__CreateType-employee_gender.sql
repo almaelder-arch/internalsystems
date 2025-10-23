b0779fb13058a59a56bd23ec0a2b47233e68b4d6
Alma Elder <almaelder83@gmail.com>
2025-10-23 10:47:13 +0100

    Making type creation re-runnable

--- employees/V0.0.02__CreateType-employee_gender.sql
+++ employees/V0.0.02__CreateType-employee_gender.sql
@@ -1,7 +1,7 @@
-BEGIN;
--- Type: employee_gender
-
-CREATE TYPE employees.employee_gender AS ENUM
-    ('M', 'F');
-
-END;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type pt JOIN pg_namespace pns ON pt.typnamespace = pns.oid WHERE pt.typname ='employee_gender' AND pns.nspname = 'employees'; ) THEN
+		CREATE TYPE employees.employee_gender AS ENUM
+		('M', 'F');
+	END IF;
+END$$

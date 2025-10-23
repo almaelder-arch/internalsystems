ce9880596b8b29ab3ecb57e7ac6f1b9d8cbb9aa5
Alma Elder <almaelder83@gmail.com>
2025-10-23 11:01:41 +0100

    fixing another syntax issue

--- employees/V0.0.03__CreateTable-employee.sql
+++ employees/V0.0.03__CreateTable-employee.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS employees.employee
     gender employees.employee_gender NOT NULL,
     hire_date date NOT NULL,
     CONSTRAINT idx_16988_primary PRIMARY KEY (id)
-)
+);
 
 ALTER SEQUENCE employees.id_employee_seq OWNED BY employees.employee.id;
 
